45e3acbd1f831c5a824a0b5ab79e8aa1fb493021
Chiranji Ranaweera <103511410+chiranji01@users.noreply.github.com>
2026-08-13 20:40:27 +1000

    Configure Django path for recommendation engine integration

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -21,10 +21,14 @@ import os
 
 
 from pathlib import Path
-
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+PROJECT_ROOT = BASE_DIR.parent.parent
+sys.path.append(str(PROJECT_ROOT))
 
 
 # Quick-start development settings - unsuitable for production
