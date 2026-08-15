1744a0d09c2bc467605d5cd265e3810247b2a5ca
Meghan Reddy Podduturi <meghanpodduturi@gmail.com>
2026-08-15 18:12:04 +1000

    register page and homeui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/home" element={<Home />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
