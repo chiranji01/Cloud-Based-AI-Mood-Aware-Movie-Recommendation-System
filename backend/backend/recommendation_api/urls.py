4a1b2809bd2b72539ba5b11b5b966af81f32ac08
Chiranji Ranaweera <103511410+chiranji01@users.noreply.github.com>
2026-08-17 17:06:42 +1000

    Add MoodFlix recommendation API demo

--- backend/backend/recommendation_api/urls.py
+++ backend/backend/recommendation_api/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 from . import views
 
+
 app_name = "recommendation_api"
 
+
 urlpatterns = [
+
+    # AI recommendation API
     path(
         "recommendations/",
         views.mood_recommendations,
         name="mood_recommendations"
+    ),
+
+    # Temporary AI demonstration page
+    path(
+        "demo/",
+        views.recommendation_demo,
+        name="recommendation_demo"
     ),
 ]
