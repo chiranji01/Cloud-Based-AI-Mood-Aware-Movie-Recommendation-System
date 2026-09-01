a80e58017fee2108001b63df2239dc0665ec880c
Meghan Reddy Podduturi <meghanpodduturi@gmail.com>
2026-09-01 19:33:40 +1000

    meghan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Ratings from "./pages/ratings/Rating";
 import MovieDetails from "./pages/movie details/movie details";
 import Mood from "./pages/moodPage/mood";
 import Profile from "./pages/profile/profile";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
